03da78cc911eafcb85e08054c9743a253db4e5e0
fabiolayerim <fabiolayerim@hotmail.com>
2024-05-23 00:42:22 -0400

    fix: no console

--- src/components/selectTimeAppointment.js
+++ src/components/selectTimeAppointment.js
@@ -20,8 +20,7 @@ function SelectTimeAppointment({
 }) {
 
 
-  const handleAvailabilityBlockSelect = async (block) => {
-    console.log("block", block);
+  const handleAvailabilityBlockSelect = async (block) => {    
     setState((state) => ({
       ...state,
       block: block,
