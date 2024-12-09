ad8a759fafe025c6077df065fae85c7bdae3fda0
GContrerasBb <gerardocontreras@hamalsolutions.com>
2024-12-09 09:17:44 -0500

    SHOW MASSAGE

--- src/App.js
+++ src/App.js
@@ -348,7 +348,7 @@ function App() {
 						setUltrasounds(ultrasounds);
 						setConsultedUltrasounds(ultrasoundsData.services);
 
-						if (state.siteId === "557418" || state.siteId === "902886" || state.siteId === "5721382" || state.siteId === "5721159" || state.siteId === "888809") {
+						if (state.siteId === "5723785" || state.siteId === "557418" || state.siteId === "902886" || state.siteId === "5721382" || state.siteId === "5721159" || state.siteId === "888809") {
 							filterMassageData.forEach((item) => {
 								const mutableItem = {
 									value: item.sessionTypeId,
